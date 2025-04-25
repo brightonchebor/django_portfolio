f205a223e140a012da0d083856e6ad36f7dc4d58
brightonchebor <cheborbrighton805@gmail.com>
2025-04-25 19:18:08 +0300

    cn

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def home(request):
     context = {}
 
     return render(request, 'base.html', context)
+
+def login(request):
+    context = {}
+
+    return render(request, 'login.html', context)
+
+def messages(request):
+    context = {}
+
+    return render(request, 'messages.html', context)
+
+def contact(request):
+    context = {}
+
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {}
+
+    return render(request, 'about.html', context)
