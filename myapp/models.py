eb318c68c01b559164f8632bae574d516a10b9ad
brightonchebor <cheborbrighton805@gmail.com>
2025-04-26 13:09:30 +0300

    cn

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ContactMe(models.Model):
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    subject = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
